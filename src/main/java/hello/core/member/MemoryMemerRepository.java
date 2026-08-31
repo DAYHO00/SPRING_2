29079c995170b5742793309652932186d7c8a313
Day One of <137034764+DAYHO00@users.noreply.github.com>
2026-08-31 18:49:44 +0900

    회원 가입 로직 개발

---
+++ src/main/java/hello/core/member/MemoryMemerRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class MemoryMemerRepository implements MemberRepository{
+
+    private static Map<Long, Member>  store = new Hashtable<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(),member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
